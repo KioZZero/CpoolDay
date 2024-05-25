5c4ba84feccf900c3150bbf43c79745ffe326bce
Cyprien <108902840+KioZZero@users.noreply.github.com>
2024-05-25 13:25:57 +0000

    my_char_replace.c

---
+++ my_char_replace.c
@@ -0,0 +1,14 @@
+void print_char(char c);
+
+
+char *my_char_replace(char *origin, char toFind, char toReplace){
+    int i = 0;
+    while(origin[i]){
+        
+        if(origin[i]==toFind){
+            print_char(toReplace);}
+        else{
+            print_char(origin[i]);}
+        i++;
+    }
+}
